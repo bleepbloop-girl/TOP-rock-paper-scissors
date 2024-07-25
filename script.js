5508e011c63546b0bebc2bec90fbf6e71852da17
bleepbloop-girl <remygtyndall@gmail.com>
2024-07-25 17:36:32 -0400

    Make both players' choice logic return strings

--- script.js
+++ script.js
@@ -1,49 +1,47 @@
 // STEP 2: Get the computer choice
 
-// Randomly return one of three strings each time the function is called
 function getComputerChoice() {
-    return Math.floor(Math.random() * 3);
+    const choice = Math.floor(Math.random() * 3);
+    switch (choice) {
+        case 0: 
+            return 'rock'; 
+            break; 
+        case 1: 
+            return 'paper'; 
+            break; 
+        case 2: 
+            return 'scissors'; 
+            break;
+    }
 }
 console.log(`Computer: ${getComputerChoice()}`); 
 
 
 // STEP 3: Get the human choice
 
-// Initialize a variable 'choice' and set it to the user input
-// Make the user input case insensitive
-const choice = prompt('Rock, Paper, Scissors... (use key groups 1/2/3 or 8/9/0 to play)'); 
+const choice = prompt('Rock, Paper, Scissors... [enter your choice below] ...SHOOT!'); 
 
-// Take a single argument 'choice' and return one of three strings if a condition is satisfied
 function getHumanChoice(choice) {
-    switch (choice) {
-        case 'rock': 
-            return 0;
-            break; 
-        case 'paper': 
-            return 1;
-            break; 
-        case 'scissors':
-            return 2;
-            break;
-        default: 
-            return `Please enter "rock", "paper", or "scissors"`
-    }
+    choice = choice.toLowerCase(); 
+    if (
+        choice === 'rock' || 
+        choice === 'paper' || 
+        choice === 'scissors'
+    ) {
+        return choice; 
+    } else return 'Please check your spelling and try again!'
 }
 console.log(`Human: ${getHumanChoice(choice)}`); 
 
+
 // STEP 4: Declare the player score variables
 
-// Create two variables to store the score of each player, and initialize the value of each variable to 0
 let humanScore = 0; 
 let computerScore = 0;
 
-
-// STEP 5: 
-
-// Take the human choice and the computer choice and increment the winner's score by 1, logging a winner announcement
 function playRound(humanChoice, computerChoice) {
-    // Make the human choice argument case insensitive
-    humanChoice = humanChoice.toLowerCase();
+    
+    
+    return `The human choice is ${humanChoice} and the computer choice is ${computerChoice}`; 
 }
-
-console.log(playRound(choice, getComputerChoice)); 
+console.log(playRound(getHumanChoice(choice), getComputerChoice())); 
