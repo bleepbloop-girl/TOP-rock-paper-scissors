093f7366447b5e806991da7d2355794c5e997f44
bleepbloop-girl <remygtyndall@gmail.com>
2024-07-26 21:18:19 -0400

    Write logic to get the human choice

--- script.js
+++ script.js
@@ -4,13 +4,32 @@ function getComputerChoice() {
     // Store a random value between 0 and 2 (inclusive) in the variable “choice”
     const choice = Math.floor(Math.random() * 3);
 
-    // If “choice” is 0, return the string “rock”
+    // If “choice” equals 0, return the string “rock”
     if (choice === 0) {
         return 'rock';
-    // If “choice” is 1, return the string “paper”
+    // If “choice” equals 1, return the string “paper”
     } else if (choice === 1) {
         return 'paper';
-    // Else return the string, “scissors”
+    // Or else return the string, “scissors”
     } else return 'scissors';
 
-} console.log(getComputerChoice()); 
+} console.log(getComputerChoice());
+
+
+// Create a function named “getHumanChoice”
+function getHumanChoice() {
+    
+    // Store user input in the variable “choice”
+    const choice = prompt('Rock, Paper, Scissors... [enter your choice] ...SHOOT!');
+
+    // If “choice” equals one of the strings “rock”, “paper”, or “scissors”, return choice 
+    if (
+        choice === 'rock' ||
+        choice === 'paper' ||
+        choice === 'scissors'
+    ) {
+        return choice;
+    // Or else return the string, "oops!" 
+    } else return 'oops'
+
+} console.log(getHumanChoice());
