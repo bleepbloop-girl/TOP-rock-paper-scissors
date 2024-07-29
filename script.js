9c226c33eca0e4d1b700ab7131381e31361a9ef1
bleepbloop-girl <remygtyndall@gmail.com>
2024-07-29 15:25:17 -0400

    Create function playRound and write pseudocode comments

--- script.js
+++ script.js
@@ -19,8 +19,8 @@ function getComputerChoice() {
 // Create a function named “getHumanChoice”
 function getHumanChoice() {
     
-    // Store user input in the variable “choice”
-    const choice = prompt('Rock, Paper, Scissors... [enter your choice] ...SHOOT!');
+    // Store user input in the variable “choice”, make input case insensitive
+    const choice = prompt('Rock, Paper, Scissors... [enter your choice] ...SHOOT!').toLowerCase();
 
     // If “choice” equals one of the strings “rock”, “paper”, or “scissors”, return choice 
     if (
@@ -30,7 +30,7 @@ function getHumanChoice() {
     ) {
         return choice;
     // Or else return the string, "oops!" 
-    } else return 'oops'
+    } else alert('Please check your spelling and try again!'); 
 
 } console.log(getHumanChoice());
 
@@ -38,3 +38,18 @@ function getHumanChoice() {
 // Create two new variables “humanScore” and “computerScore” in the global scope
 let humanScore = 0;
 let computerScore = 0; // Initialize both variables with the value 0
+
+
+// Create a function named “playRound” with two parameters: “humanChoice” and “computerChoice”
+function playRound(humanChoice, computerChoice) {
+     
+// If both players’ choices are equal, increment neither score and log: “It’s a tie!”
+
+// If humanChoice is “paper” and computerChoice” is “rock”, increment humanScore by 1 and log: “You win! Paper beats Rock!”
+
+// If humanChoice is “scissors” and computerChoice is “paper”, increment humanScore by 1 and log: “You win! Scissors beats Paper!” 
+
+// If humanChoice is “rock” and computerChoice is “scissors”, increment humanScore by 1 and log: “You win! Rock beats Scissors!” 
+
+// Else, increment computerScore by 1 and log: “You lose! (computerChoice) beats (humanChoice)”
+}
