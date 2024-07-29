e67d3ab1e98588bcf76d839db91fc07ce26fdc4b
bleepbloop-girl <remygtyndall@gmail.com>
2024-07-29 17:43:01 -0400

    Write logic to play a single round

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function getComputerChoice() {
     // Or else return the string, “scissors”
     } else return 'scissors';
 
-} console.log(getComputerChoice());
+}
 
 
 // Create a function named “getHumanChoice”
@@ -32,16 +32,41 @@ function getHumanChoice() {
     // Or else return the string, "oops!" 
     } else alert('Please check your spelling and try again!'); 
 
-} console.log(getHumanChoice());
+}
 
 
 // Create two new variables “humanScore” and “computerScore” in the global scope
 let humanScore = 0;
-let computerScore = 0; // Initialize both variables with the value 0
-console.log(`The human score is ${humanScore} and the computer score is ${computerScore}`); 
+let computerScore = 0; // Initialize both variables with the value 0 
 
 
 // Create a function named “playRound” with two parameters: “humanChoice” and “computerChoice”
 function playRound(humanChoice, computerChoice) {
-    
+    console.log(`The human choice is ${humanChoice} and the computer choice is ${computerChoice}`);
+
+    // If both players’ choices are equal, increment neither score and log: “It’s a tie!”
+    if (humanChoice === computerChoice) {
+        console.log(`It's a tie!`);
+        // If humanChoice is “rock” and computerChoice is “scissors”, increment humanScore by 1 and log: “You win! Rock beats Scissors!”  
+    } else if (humanChoice === 'rock' && computerChoice === 'scissors') {
+        console.log('You win! Rock beats Scissors!'); 
+        humanScore += 1; 
+        // If humanChoice is “paper” and computerChoice” is “rock”, increment humanScore by 1 and log: “You win! Paper beats Rock!”
+    } else if (humanChoice === 'paper' && computerChoice === 'rock') {
+        console.log('You win! Paper beats Rock!');
+        humanScore += 1;
+        // If humanChoice is “scissors” and computerChoice is “paper”, increment humanScore by 1 and log: “You win! Scissors beats Paper!”  
+    } else if (humanChoice === 'scissors' && computerChoice === 'paper') {
+        console.log('You win! Scissors beats Paper!'); 
+        humanScore += 1; 
+    } else {
+        // Else, increment computerScore by 1 and log: “You lose! [value of computerChoice] beats [value of humanChoice]!”
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}!`); 
+        computerScore += 1;
+    }
 }
+
+playRound(getHumanChoice(), getComputerChoice());  
+
+console.log(`Human: ${humanScore}`); 
+console.log(`Computer: ${computerScore}`);
