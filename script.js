036e4ebbe381362a3da5d94583844bce784de421
bleepbloop-girl <remygtyndall@gmail.com>
2024-07-29 18:15:44 -0400

    Write logic to play the entire game

--- script.js
+++ script.js
@@ -35,39 +35,42 @@ function getHumanChoice() {
 }
 
 
-// Create two new variables “humanScore” and “computerScore” in the global scope
-let humanScore = 0;
-let computerScore = 0; // Initialize both variables with the value 0 
-
-
-// Create a function named “playRound” with two parameters: “humanChoice” and “computerChoice”
-function playRound(humanChoice, computerChoice) {    
-    if (humanChoice === computerChoice) {
-        // If both players’ choices are equal, increment neither score and log: “It’s a tie!”
-        console.log(`It's a tie!`);
-    } else if (
-        // If the choices pass any of the following conditions, run the following block of code
-        humanChoice === 'rock' && computerChoice === 'scissors' ||
-        humanChoice === 'paper' && computerChoice === 'rock' ||
-        humanChoice === 'scissors' && computerChoice === 'paper'
-    ) {
-        // Log a winner announcement, increment "humanScore" by 1
-        console.log(`you win! ${humanChoice} beats ${computerChoice}!`); 
-        humanScore += 1; 
-    } else {
-        // Log an alternative winner announcment, increment "computerScore" by 1
-        console.log(`you lose! ${computerChoice} beats ${humanChoice}!`); 
-        computerScore += 1;
-    }
-} playRound(getHumanChoice(), getComputerChoice());  
-
-console.log(`Human: ${humanScore}`); 
-console.log(`Computer: ${computerScore}`);
-
-
 // Create a function named “playGame” 
 function playGame() {
-    // Move playRound and score variables so that they’re declared inside of playGame
+
+    // Create two new variables “humanScore” and “computerScore” in the global scope
+    let humanScore = 0;
+    let computerScore = 0; // Initialize both variables with the value 0 
+
+    // Create a function named “playRound” with two parameters: “humanChoice” and “computerChoice”
+    function playRound(humanChoice, computerChoice) { 
+
+        // If both players’ choices are equal, increment neither score and log: “It’s a tie!”
+        if (humanChoice === computerChoice) {
+            console.log(`it's a tie!`);
+
+        // If the choices pass any of the following conditions, run the following block of code
+        } else if (
+            humanChoice === 'rock' && computerChoice === 'scissors' ||
+            humanChoice === 'paper' && computerChoice === 'rock' ||
+            humanChoice === 'scissors' && computerChoice === 'paper'
+        ) {
+            // Log a winner announcement, increment "humanScore" by 1
+            console.log(`you win! ${humanChoice} beats ${computerChoice}!`); 
+            humanScore += 1; 
+
+        // Or else log an alternative winner announcment, increment "computerScore" by 1
+        } else {
+            console.log(`you lose! ${computerChoice} beats ${humanChoice}!`); 
+            computerScore += 1;
+        }
+    } 
 
     // Play 5 rounds by calling playRound 5 times
+    for (i = 0; i < 5; i ++) {
+        playRound(getHumanChoice(), getComputerChoice());
+        console.log(`HUMAN: ${humanScore} \nCOMPUTER: ${computerScore} \n `); 
+    }
 }
+
+playGame();
